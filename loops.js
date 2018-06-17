97f4987fdec5b35484fcbc49d52c564f629d4304
scalande@yahoo.com <scalande@yahoo.com>
2018-06-17 20:08:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,6 +11,16 @@ function forLoop(array){
 }
 
 
+function forLoop(array) {
+  for ( let i = 0; i < 25; i++) {
+    if (i === 1) {
+     } else {
+       array.push(`I am ${i} strange loops.`)
++     }
++  }
++  return array;
++}
+
 
 function whileLoop(n){
   let countdown = n
