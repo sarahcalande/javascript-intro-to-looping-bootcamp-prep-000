92c705812e61bc5a8b63828655c2c4094d4885d7
scalande@yahoo.com <scalande@yahoo.com>
2018-06-17 20:08:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,25 +1,16 @@
-function forLoop(array){
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-     return "I am 1 strange loop.";
-    } else {
-      return `I am ${i} strange loops.`;
-    }
-    
-    return array;
-}
-}
 
 
 function forLoop(array) {
   for ( let i = 0; i < 25; i++) {
     if (i === 1) {
-     } else {
+      array.push(`I am 1 strange loop.`)
+      
+    } else {
        array.push(`I am ${i} strange loops.`)
-+     }
-+  }
-+  return array;
-+}
+     }
+  }
+  return array;
+}
 
 
 function whileLoop(n){
